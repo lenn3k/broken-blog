cc0bb92e1ed655702fcbcf3b39c16ebd838d0406
Johan Silkens <me@jsilkens.be>
2018-04-13 10:15:00 +0200

    Add keycloak and security config
    TODO fix csrf!

---
+++ src/main/java/be/ordina/blog/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package be.ordina.blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter  {
+
+    //Submit the Keycloak Auth provider to the auth manager
+    @Autowired
+    public void ConfigureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    // Specifies the session auth strategy
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.PUT,"/api/v1/*")
+                .hasRole("user")
+                .anyRequest()
+                .permitAll();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.DELETE,"/api/v1/*")
+                .hasRole("user")
+                .anyRequest()
+                .permitAll();
+        //insecurity everywhere!
+        http.csrf().disable();
+        // h2 console won't load without this option
+        http.headers().frameOptions().disable();
+    }
+}
