3a6906d65237adedd5183a77baf6eb1cffafd0a5
Johan Silkens <me@jsilkens.be>
2018-04-20 21:29:45 +0200

    Fix merge issue

--- src/main/java/be/ordina/blog/config/SecurityConfig.java
+++ src/main/java/be/ordina/blog/config/SecurityConfig.java
@@ -68,11 +68,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter  {
                 .hasRole("")
                 .anyRequest()
                 .permitAll();
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/api/v1/*")
-                .hasRole("user")
-                .anyRequest()
-                .permitAll();
         //insecurity everywhere!
         http.csrf().disable();
         // h2 console won't load without this option
